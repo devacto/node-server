596dc1a3ba9ed1997c6cef8bf9aef57b242f1814
Victor Wibisono <vwibisono@dius.com.au>
2015-10-13 14:51:35 +1100

    Retrieves data from open weather map.

--- routes/weather.js
+++ routes/weather.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('http');
+var logger = require('morgan');
+var rp = require('request-promise');
+require('request-promise').debug = true;
+
+// App ID for openweathermap
+var APP_ID = '8466917d0340021cf7baba0b7c90ef9';
+var CITY = 'Melbourne';
+
+var options = {
+  uri: 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY + '&APPID=' + APP_ID,
+  json: true
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('weather');
+  rp(options)
+    .then(function(body) {
+      res.send(body);
+    })
+    .catch(function(err) {
+      res.status(err.statusCode).send(err);
+    });
 });
 
 module.exports = router;
